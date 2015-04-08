e5532339fa235700ee1c14741dfb433f13d61f3f
ANIL PACHHAI <anilpachhai@ANILs-MacBook-Pro.local>
2015-04-07 21:08:41 -0700

    slot machine press spin

---
+++ Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by ANIL PACHHAI on 4/6/15.
+//  Copyright (c) 2015 Palpa Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
